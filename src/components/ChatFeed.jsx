8cc570c0455534202e726f33f83c11025e3bfa8f
Mheyrie <omota4fidelity@gmail.com>
2024-08-08 21:18:35 +0100

    Creating chat feed logic

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from "react";
+import ThierMessage from "./ThierMessage";
+import MyMessage from "./MyMessage";
+import MessageForm from "./MessageForm";
 
 const ChatFeed = (props) => {
-    console.log(props)
-  return (
-    <div>ChatFeed</div>
-  )
-}
+  const { chats, activeChat, userName, messages } = props;
 
-export default ChatFeed
+  const chat = chats && chats[activeChat];
+
+  const rendermessages = () => {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.userName;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? <MyMessage message={message} /> : <ThierMessage message={message} lastMessage={lastMessageKey} />}
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <ThierMessage />
+      <MyMessage />
+      <MessageForm />
+    </div>
+  );
+};
+
+export default ChatFeed;
