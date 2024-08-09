ac93cf36739d67718ee2a2d2125849179d4a26ae
Mheyrie <omota4fidelity@gmail.com>
2024-08-10 00:13:26 +0100

    Rendering their message component

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const ThierMessage = () => {
+const ThierMessage = ({ message, lastMessage }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+
   return (
-    <div>ThierMessage</div>
-  )
-}
+    <div className="message-role">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
+        />
+      )}
+      {message.attachments && message.attachments.length > 0 ? (
+        <img
+          src={message.attachment[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        (
+          <div
+          className="message"
+          style={{
+            float: "left",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+            color: "white",
+            backgroundColor: "#CADCDC",
+          }}
+        >
+          {message.text}
+        </div>
+        )
+      )}
+    </div>
+  );
+};
 
-export default ThierMessage
+export default ThierMessage;
