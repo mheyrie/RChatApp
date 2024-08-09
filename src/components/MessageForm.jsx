e65c4e73880d4858bb069e6d0d30d172a9d1003d
Mheyrie <omota4fidelity@gmail.com>
2024-08-10 00:41:44 +0100

    Working on Message field

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { SendOutlined, PictureOutlined } from "@ant-design/icons"
+import { sendMessage, isTyping } from "react-chat-engine"
 
-const MessageForm = () => {
+const MessageForm = (props) => {
+
+  const [value, setValue] = useState('')
+  const {chatIde, creds} = props;
+
   return (
-    <div>MessageForm</div>
+    <div className="message-form" onSubmit={handleSubmit}>
+      <input 
+      className="message-input" 
+      placeholder="Send a message" 
+      value={value}
+      onChange={handleChange}
+      onSubmit={handleSubmit} 
+    />
+    <label htmlFor="upload-button">
+      <span  className="image-button">
+        <PictureOutlined className="picture-icon" />
+      </span>
+    </label>
+    <input 
+    type="file" 
+    multiple={false}
+    id="upload-button"
+    />
+    </div>
   )
 }
 
